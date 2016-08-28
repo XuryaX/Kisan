77586af72c6011684da407347f0cd6ba42752907
xuryax <shauryadeepc@hotmail.com>
2016-08-28 18:43:20 +0530

    Updating Settings

--- Kisan/settings.py
+++ Kisan/settings.py
@@ -76,16 +76,14 @@ WSGI_APPLICATION = 'Kisan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fkkvspaz',
+        'USER': 'fkkvspaz',
+        'PASSWORD': 'MwTWlQHScGtnG72qqHjOSqR_C8IrqlbZ',
+        'HOST': 'elmer-01.db.elephantsql.com',
+        'PORT': '',
     }
 }
-
-import dj_database_url
-
-db_from_env = dj_database_url.config()
-
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
