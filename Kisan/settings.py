6ef602a7db1641998dd08e4248218e8bfb91c899
xuryax <shauryadeepc@hotmail.com>
2016-08-28 18:09:35 +0530

    DB Config

--- Kisan/settings.py
+++ Kisan/settings.py
@@ -73,14 +73,13 @@ WSGI_APPLICATION = 'Kisan.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
 DATABASES = {}
 
