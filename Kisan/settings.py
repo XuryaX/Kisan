2a5c0915c624249373956ab41ceafd6f6f97a9d0
xuryax <shauryadeepc@hotmail.com>
2016-08-28 18:23:46 +0530

    Updating Settings

--- Kisan/settings.py
+++ Kisan/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "cssjs")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "cssjs"),
-)
 
 STATIC_URL = '/static/'
 
